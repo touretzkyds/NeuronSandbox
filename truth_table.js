6e6ead759c916109128e161c381065d2ed7c9f8c
Angela <45048476+achenvc@users.noreply.github.com>
2023-03-06 14:50:59 -0500

    fixed desired output resetting when binary is toggled
    ui changes

--- truth_table.js
+++ truth_table.js
@@ -1,9 +1,30 @@
+function saveDesiredValues() {
+    desired_values = []
+    const outputTable = document.getElementById("output-table");
+    for(i = 1; i < outputTable.rows.length; i++) {
+        desired_values.push(outputTable.rows[i].cells[2].innerText);
+    }
+    return desired_values
+}
+
+function restoreDesiredValues( desired_values)
+{
+    const outputTable = document.getElementById("output-table");
+    for(i = 1; i < outputTable.rows.length; i++) {
+        outputTable.rows[i].cells[2].innerText = desired_values[i-1];
+    }
+}
+
 function setupGenerateTruthTable() {
     vars = []
     // document.getElementById('generateTruthTable').addEventListener('click', ()=>{
+        //save desired value before update
+        let desire_values = saveDesiredValues();
         var truthData = dataOp.createBinaryData(inputTable.numCols);
         demo.removeAllInputDataRows(false);
         writeTruthTable(truthData);
+        restoreDesiredValues(desire_values);
+        //restore desired value
     // });
 }
 
